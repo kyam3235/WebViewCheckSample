ccae83555b07df4a5b1b1548e820730d1e4e88db
Kyohei Yamaguchi <kyohei3235@gmail.com>
2021-03-11 23:23:54 +0900

    create mainactivity

--- app/src/main/java/jp/kyamlab/webviewchecksample/MainActivity.kt
+++ app/src/main/java/jp/kyamlab/webviewchecksample/MainActivity.kt
@@ -2,10 +2,14 @@ package jp.kyamlab.webviewchecksample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.web_view)
+        webView.loadUrl("file:///android_asset/sample.html")
     }
 }
